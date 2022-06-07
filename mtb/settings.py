297b7ba9b9271ab26b0da5f05a8488c9c7fb2792
Hejiaming <sqming9527@vip.qq.com>
2022-06-07 16:26:46 +0800

    run

--- mtb/settings.py
+++ mtb/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-2o-c_s^rc#+i#qz#&lrw#a(17cih7^!t8f!facu91%ug2a5#lv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 

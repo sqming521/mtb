b9d2b11986071e64c3c7de9d42f946d21050fea1
Hejiaming <sqming9527@vip.qq.com>
2022-06-11 01:37:26 +0800

    状态码

--- utils/extension/return_code.py
+++ utils/extension/return_code.py
@@ -1,6 +1,12 @@
 # 成功
 SUCCESS = 0
 
+# 失败
+ERROR = 100
+
+# 用户提交数据校验失败（字段错误）
+FIELD_ERROR = 1000
+
 # 用户提交数据校验失败
 VALIDATE_ERROR = 1001
 
@@ -12,7 +18,6 @@ AUTH_OVERDUE = 2001
 
 # 无权访问
 PERMISSION_DENIED = 3000
-
 
 # 无权访问
 TOO_MANY_REQUESTS = 4000

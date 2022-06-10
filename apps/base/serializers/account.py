f55ab121d22f5e196c91f0a2a5eab302a9aa9781
Hejiaming <sqming9527@vip.qq.com>
2022-06-11 02:32:00 +0800

    xiufu

--- apps/base/serializers/account.py
+++ apps/base/serializers/account.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 class AuthSerializer(serializers.Serializer):
     username = serializers.CharField(label="用户名",required=True)
-    password = serializers.CharField(label="密码",required=True)
+    password = serializers.CharField(label="密码",required=True,min_length=6)
--- mtb/settings.py
+++ mtb/settings.py
@@ -42,11 +42,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'utils.middlewares.cors.CorsMiddleware'
+    'utils.middlewares.cors.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     # 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
